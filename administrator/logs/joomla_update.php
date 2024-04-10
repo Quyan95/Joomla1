169ff10178b7a22c0f8bfc2fcbbefd85c5062f67
quyan <ravshan_1995@bk.ru>
2024-04-10 16:14:58 +0300

    help me

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-02-21 13:39:16 UTC
+#Software: Joomla! 5.0.0 Stable [ Kuboresha ] 17-October-2023 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-02-21T13:39:16+00:00	WARNING 127.0.0.1	jerror	Update: :Extension: Не удалось открыть https://update.joomla.org/core/j5/default.xml.
